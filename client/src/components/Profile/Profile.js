c3591b113a83a2e8a221e9cc7b731548ba52e97d
kevinsherrell <k.sherrell.1028@gmail.com>
2019-04-25 02:05:42 -0500

    added more ui for dashboard

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -6,7 +6,7 @@ class Profile extends Component {
             <div>
                 {/* begin authenticated header */}
                 <header>
-                    <div className='profileHeaderWrapper '>
+                    <div className='profileWrapper '>
                         <div className='profileHeaderGridContainerLeft'>
                             <div className="notFacebookLogo">
                                 <h2 className='notFacebookText'>
@@ -28,11 +28,54 @@ class Profile extends Component {
                             </div>
                             <p className="home">Home</p>
                             <p className="create">Create</p>
-                            <p className="communications"><span><i class="fas fa-user-friends"></i></span><span><i class="fab fa-facebook-messenger"></i></span><span><i class="fas fa-bell"></i></span></p>
+                            <p className="communications"><span className='friends'><i class="fas fa-user-friends"></i></span><span className='messenger'><i class="fab fa-facebook-messenger"></i></span><span className='notifications'><i class="fas fa-bell"></i></span></p>
+                            <p className="help"><span><i class="fas fa-question-circle"></i></span></p>
+                            <p className='headerDropDown'><span><i class="fas fa-caret-down"></i></span></p>
                         </div>
+                        {/* end HEADER */}
+                        {/* begin BODY */}
 
                     </div>
                 </header>
+                <div className='profileBodyWrapper'>
+                    <div className='profileBody'>
+                        <div className="left">
+                            <div className='leftAvatarContainer'>
+                                <div className='leftAvatar'>
+                                    <span>avatar</span>
+                                </div>
+                            <p>name</p>
+
+                            </div>
+                        </div>
+                        <div className="mid">
+                            <div className="createPost">
+
+                                <div className='postHeader'>
+                                    <p>Create Post</p>
+                                </div>
+                                <div className='postContainer'>
+                                    <div className="postBody">
+                                        <div className='postBodyAvatarContainer'>
+                                            <div className="postBodyAvatar">
+                                                <span>avatar</span>
+                                            </div>
+                                        </div>
+                                        <div className='postBodyInput'>
+                                            <input type="text" placeholder="What's on your mind, person?" />
+                                        </div>
+                                    </div>
+                                    <div className="postOptions">
+                                        
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className="right"></div>
+                    </div>
+                </div>
             </div>
         );
     }
