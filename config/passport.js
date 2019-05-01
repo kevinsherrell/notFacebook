7d4be189e16537ec524751a6d48f2be3b9cd3863
kevinsherrell <k.sherrell.1028@gmail.com>
2019-05-01 12:11:46 -0500

    ...

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
 const mongoose = require('mongoose')
-const User = require('../models/User')
+const User = require('../models/User.js')
 
 const keys = require('../config/keys')
 
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose.connect(mongoURI, { useNewUrlParser: true }, () => {
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/posts', require('./routes/api/posts'))
+
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
