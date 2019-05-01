60a75483b2cecbdd92bc15808894e5830c006cde
kevinsherrell <k.sherrell.1028@gmail.com>
2019-05-01 13:05:49 -0500

    ...

--- config/passport.js
+++ config/passport.js
@@ -3,9 +3,9 @@ dotenv.config();
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
 const mongoose = require('mongoose')
-const User = require('../models/User.js')
+const User = require('../models/User')
 
-const keys = require('../config/keys_prod').secretOrKey
+const keys = require('../config/keys')
 // 
 const opts = {}
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
