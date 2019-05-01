e2bc33f23c41b371cd0b964680a138d98939d9bf
kevinsherrell <k.sherrell.1028@gmail.com>
2019-05-01 13:27:52 -0500

    ...

--- config/passport.js
+++ config/passport.js
@@ -5,6 +5,7 @@ const ExtractJwt = require('passport-jwt').ExtractJwt
 const mongoose = require('mongoose')
 const User = require('../models/User')
 
+
 const keys = require('../config/keys')
 // 
 const opts = {}
@@ -15,11 +16,14 @@ module.exports = passport => {
     passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
         User.findById(jwt_payload.id)
             .then(user => {
+                console.log(opts)
                 if (user) {
                     return done(null, user)
                 }
                 return done(null, false)
+                
             })
             .catch(err => console.log(err))
     }))
+    
 }
