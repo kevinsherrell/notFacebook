65817b1937afc071cdca1c64bee8623bbebbdd85
kevinsherrell <k.sherrell.1028@gmail.com>
2019-05-02 05:20:00 -0500

    ...

--- client/src/store.js
+++ client/src/store.js
@@ -9,18 +9,18 @@ const initialState = {}
 const middleware = [thunk]
 
 // const devTools = process.env.NODE_ENV === 'development' ?  : null
-const store = createStore(
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    ))
-
-export default store
-
-
-// Working
-// const store = createStore(rootReducer,initialState,composeWithDevTools(applyMiddleware(thunk))
-// )
+// const store = createStore(
+//     rootReducer,
+//     initialState,
+//     compose(applyMiddleware(...middleware),
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     ))
 
 // export default store
+
+
+Working
+const store = createStore(rootReducer,initialState,composeWithDevTools(applyMiddleware(thunk))
+)
+
+export default store
