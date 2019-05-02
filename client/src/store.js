a69b6da27d5b9f61cb1347e5ac95912fcba6613b
kevinsherrell <k.sherrell.1028@gmail.com>
2019-05-01 21:19:19 -0500

    ...

--- client/src/store.js
+++ client/src/store.js
@@ -5,11 +5,12 @@ import rootReducer from './reducers'
 const initialState = {}
 const middleware = [thunk]
 
+const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : null
 const store = createStore(
-    rootReducer, 
+    rootReducer,
     initialState,
     compose(applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        devTools
     ))
 
 export default store
